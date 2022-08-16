c01ddf6aa2e5be269acc1322f4f2b4e4f5ba55f0
omnisats <omnisats@protonmail.com>
2022-08-16 12:37:52 +0200

    add client and client_tools

---
+++ client/client.py
@@ -0,0 +1,108 @@
+
+
+import json
+import os
+import asyncio
+from starkware.starknet.public.abi import get_selector_from_name
+from starknet_py.net.signer.stark_curve_signer import KeyPair, StarkCurveSigner
+from starknet_py.net.networks import MAINNET, TESTNET
+from starknet_py.net.models import StarknetChainId
+from starknet_py.net.gateway_client import GatewayClient
+from starknet_py.net.client_models import Call
+from starknet_py.net import AccountClient
+from starknet_py.contract import Contract
+from starknet_py.net.networks import TESTNET
+from dotenv import load_dotenv
+from client_tools import fetch_coinbase, fetch_okex, prepare_contract_call_args
+
+
+load_dotenv()
+
+OPEN_ORACLE_ADDRESS = '0x01e02f5a90dd2071287160b5b3ee60e6c5b12f4be91c4e4caf424172efc53f1b'
+NETWORK = TESTNET
+
+
+class OpenOracleClient(object):
+    def __init__(
+        self,
+        account_private_key=int(os.getenv('ACCOUNT_PRIVATE_KEY')),
+        account_contract_address=os.getenv('ACCOUNT_CONTRACT_ADDRESS'),
+        open_oracle_address=OPEN_ORACLE_ADDRESS,
+        network=NETWORK
+    ):
+        if network == TESTNET:
+            self.chain_id = StarknetChainId.TESTNET
+        elif network == MAINNET:
+            self.chain_id = StarknetChainId.MAINNET
+        else:
+            raise NotImplementedError(
+                "Empiric.BaseClient: Network not recognized, unknown Chain ID"
+            )
+        self.network = network
+        self.client = GatewayClient(self.network)
+
+        # Account contract and signer
+        self.account_contract_address = account_contract_address
+        self.account_contract = None
+        self.account_private_key = account_private_key
+        self.signer = StarkCurveSigner(self.account_contract_address, KeyPair.from_private_key(
+            self.account_private_key), self.chain_id)
+        self.account_client = AccountClient(
+            self.account_contract_address, self.client, self.signer)
+        assert type(
+            account_private_key) == int, "Account private key must be integer"
+
+        # OpenOracle contract
+        open_oracle_abi = open('build/OpenOraclePublisher_abi.json', 'r')
+        self.open_oracle_contract = Contract(
+            address=open_oracle_address, abi=json.load(open_oracle_abi), client=self.client)
+        open_oracle_abi.close()
+
+    async def _fetch_base_contracts(self):
+        if self.account_contract is None:
+            self.account_contract = await Contract.from_address(
+                self.account_contract_address, self.client
+            )
+
+    async def get_balance(self):
+        return await self.account_client.get_balance()
+
+    async def send_transaction(self, to_contract, selector_name, calldata) -> hex:
+        selector = get_selector_from_name(selector_name)
+        return await self.send_transactions([Call(to_contract, selector, calldata)])
+
+    async def send_transactions(self, calls) -> hex:
+        return hex((await self.account_client.execute(calls, auto_estimate=True)).hash)
+
+    async def publish_open_oracle_entries_okex(self, assets=['btc', 'eth', 'dai']) -> hex:
+        okx_oracle_data = fetch_okex(assets=assets)
+        calls = [self.open_oracle_contract.functions["publish_entry"].prepare(
+            prepare_contract_call_args(*oracle_data)) for oracle_data in okx_oracle_data]
+
+        return await self.send_transactions(calls=calls)
+
+    async def publish_open_oracle_entries_coinbase(self, assets=['btc', 'eth', 'dai']) -> hex:
+        coinbase_oracle_data = fetch_coinbase(assets=assets)
+        calls = [self.open_oracle_contract.functions["publish_entry"].prepare(
+            prepare_contract_call_args(*oracle_data)) for oracle_data in coinbase_oracle_data]
+
+        return await self.send_transactions(calls=calls)
+
+    async def publish_open_oracle_entries_all_publishers(self, assets=['btc', 'eth', 'dai']) -> hex:
+        okx_oracle_data = fetch_okex(assets=assets)
+        coinbase_oracle_data = fetch_coinbase(assets=assets)
+        all_data = okx_oracle_data + coinbase_oracle_data
+
+        calls = [self.open_oracle_contract.functions["publish_entry"].prepare(
+            prepare_contract_call_args(*oracle_data)) for oracle_data in all_data]
+        return await self.send_transactions(calls=calls)
+
+
+async def main():
+    c = OpenOracleClient()
+    await c.publish_open_oracle_entries_okex(assets=['btc'])
+
+
+if __name__ == "__main__":
+
+    asyncio.run(main())
---
+++ client/client_tools.py
@@ -0,0 +1,111 @@
+import os
+import base64
+import datetime
+import hmac
+import requests
+from hashlib import sha256
+from typing import Tuple, List
+
+
+def to_uint(a):
+    """Takes in value, returns uint256-ish tuple."""
+    return (a & ((1 << 128) - 1), a >> 128)
+
+
+def remove_0x_if_present(eth_hex_data: str) -> str:
+    if eth_hex_data[0:2].upper() == '0X':
+        return eth_hex_data[2:]
+    else:
+        return eth_hex_data
+
+
+def fetch_okex(assets: List[str] = ['btc', 'eth', 'dai']) -> List[Tuple[str, str, str]]:
+    okx_wallet_address = '85615b076615317c80f14cbad6501eec031cd51c'  # from api docs
+
+    r = requests.get('https://www.okx.com/api/v5/market/open-oracle')
+    r_dict = r.json()['data'][0]
+    messages = r_dict['messages']
+    signatures = r_dict['signatures']
+    prices = r_dict['prices']
+
+    result = []
+    for asset in assets:
+        index = list(prices.keys()).index(asset.upper())
+        result.append((messages[index], signatures[index], okx_wallet_address))
+
+    return result
+
+
+def fetch_coinbase(assets: List[str] = ['btc', 'eth', 'dai']) -> List[Tuple[str, str, str]]:
+    coinbase_wallet_address = '0xfCEAdAFab14d46e20144F48824d0C09B1a03F2BC'
+    COINBASE_API_SECRET = os.getenv("COINBASE_API_SECRET")
+    COINBASE_API_KEY = os.getenv("COINBASE_API_KEY")
+    COINBASE_API_PASSPHRASE = os.environ.get("COINBASE_API_PASSPHRASE")
+    URL = "https://api.exchange.coinbase.com"
+    REQUEST_PATH = "/oracle"
+    METHOD = "GET"
+    headers = {
+        "Accept": "application/json",
+        "CB-ACCESS-KEY": COINBASE_API_KEY,
+        "CB-ACCESS-SIGN": base64.b64encode(signature.digest()),
+        "CB-ACCESS-TIMESTAMP": request_timestamp,
+        "CB-ACCESS-PASSPHRASE": COINBASE_API_PASSPHRASE,
+    }
+    request_timestamp = str(
+        int(
+            datetime.datetime.now(datetime.timezone.utc)
+            .replace(tzinfo=datetime.timezone.utc)
+            .timestamp()
+        )
+    )
+    signature = hmac.new(
+        base64.b64decode(COINBASE_API_SECRET),
+        (request_timestamp + METHOD + REQUEST_PATH).encode("ascii"),
+        sha256,
+    )
+    response = requests.request(
+        METHOD, URL + REQUEST_PATH, headers=headers, timeout=10
+    )
+
+    response.raise_for_status()
+    result = response.json()
+    messages = result['messages']
+    signatures = result['signatures']
+    prices = result['prices']
+
+    result = []
+    for asset in assets:
+        index = list(prices.keys()).index(asset.upper())
+        result.append(
+            (messages[index], signatures[index], coinbase_wallet_address))
+
+    return result
+
+
+def prepare_contract_call_args(oracle_message_hex: str, oracle_signature_hex: str, eth_wallet_address: str) -> dict:
+    message_bytes = bytes.fromhex(remove_0x_if_present(oracle_message_hex))
+    signature_bytes = bytes.fromhex(remove_0x_if_present(oracle_signature_hex))
+
+    timestamp_little_endian = int.from_bytes(message_bytes[56:64], 'little')
+    price_little_endian = int.from_bytes(message_bytes[120:128], 'little')
+    ticker_len_little_endian = int.from_bytes(message_bytes[216:224], 'little')
+    ticker_little_endian = int.from_bytes(message_bytes[224:232], 'little')
+
+    signature_r_big = int.from_bytes(signature_bytes[0:32], 'big')
+    signature_s_big = int.from_bytes(signature_bytes[32:64], 'big')
+    signature_v_big = int.from_bytes(signature_bytes[64:96], 'big')
+
+    signature_r_uint256 = to_uint(signature_r_big)
+    signature_s_uint256 = to_uint(signature_s_big)
+
+    eth_address_big = int(remove_0x_if_present(eth_wallet_address), 16)
+
+    if signature_v_big == 27 or signature_v_big == 28:
+        signature_v_big -= 27  # See https://github.com/starkware-libs/cairo-lang/blob/13cef109cd811474de114925ee61fd5ac84a25eb/src/starkware/cairo/common/cairo_secp/signature.cairo#L173-L174
+
+    contract_call_args = {'t_little': timestamp_little_endian, 'p_little': price_little_endian,
+                          'ticker_len_little': ticker_len_little_endian, 'ticker_name_little': ticker_little_endian,
+                          'r_low': signature_r_uint256[0], 'r_high': signature_r_uint256[1],
+                          's_low': signature_s_uint256[0], 's_high': signature_s_uint256[1],
+                          'v': signature_v_big, 'eth_address': eth_address_big}
+    return contract_call_args
